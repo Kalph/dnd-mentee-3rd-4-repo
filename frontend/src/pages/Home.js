490eeaa52863492e79b986072d42040077f11bcd
HyungJunKim <kjskhj04366@naver.com>
2020-08-17 01:53:52 +0900

    Feat setting for react

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+import { Link } from 'react-router-dom';
+import styles from './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Home;
